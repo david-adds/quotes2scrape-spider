05d2f076553beea32a97a31151e631d2470ad9d0
david-adds <55636118+david-adds@users.noreply.github.com>
2021-12-30 13:22:08 -0300

    Scrapy and JavaScript integration using Splash

--- quotes2scrape/spiders/quotes.py
+++ quotes2scrape/spiders/quotes.py
@@ -1,10 +1,29 @@
 import scrapy
+from scrapy_splash import SplashRequest
 
 
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
     allowed_domains = ['quotes.toscrape.com']
     start_urls = ['http://quotes.toscrape.com/']
+    
+    script = '''
+        function main(splash,args)
+            splash.private_mode_enable = false
+            url = args.url
+            assert(splash:go(url))
+            assert(splash:wait(1))
+            quotes = assert(splash:select_all('.row'))
+            assert(splash:wait(1))
+            splash:set_viewport_full()
+            return splash:html()
+        end
+    '''
+    def start_requests(self):
+        yield SplashRequest(url='http://quotes.toscrape.com',callback=self.parse,endpoint='execute',args={
+            'lua_source': self.script
+        })
 
     def parse(self, response):
-        pass
+        print(response.body)
+        
